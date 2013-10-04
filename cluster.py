1f1736f1c60882a4461883d489b5b859e422a3f4
Martin Rueckl <mrueckl@physik.hu-berlin.de>
2013-10-04 18:03:48 +0200

    added puffs method to cluster that allows to iterate over collaborative events of the clustered channels

--- cluster.py
+++ cluster.py
@@ -1,5 +1,4 @@
-
-
+import numpy
 
 class Cluster(object):
 	def __init__(self, index , channels, eventdata,simulation):
@@ -8,46 +7,86 @@ class Cluster(object):
 		self.__simulation = simulation
 		self.__events = None
 		self.__index  = index
-		
+		self.__puffs  = {}
+	
+	# deprecated
 	def channels(self):
 		return self._channels
 	
+	# make the cluster iterable over the channels
+	def __iter__(self):
+		for c in self._channels:
+			yield c
+	
+
+	# return iterable for iteration over puffs
+	def puffs(self, tolerance = 0.005):
+		
+		#if we do not have the data available for the asked tolerance, calculate it and store it in dictionary
+		if not self.__puffs.has_key(tolerance):
+			data = self.events()
+			
+			wasopen = data[0]['noch'] > 0
+			self.__puffs[tolerance] = []
+			
+			puffs = self.__puffs[tolerance]
+			
+			active    = False
+			lastclose = -2*tolerance
+
+			for i in range(len(data)):
+				isopen = data[i]['noch'] > 0
+					
+				#first channel opened
+				if not wasopen and isopen:
+					withintolerance = data[i]['t']-lastclose <= tolerance
+				
+					if not withintolerance:
+						self.__puffs[tolerance] = self.__puffs[tolerance] + [[i,-1]]
+			
+				#last channel closed
+				if not isopen and wasopen:
+					lastclose = data[i]['t']    
+					self.__puffs[tolerance][-1][1] = i
+				if isopen:
+					self.__puffs[tolerance][-1][1] = i
+				
+				wasopen = isopen
+		
+		# at this point we have calculated the new data, and also the puffs event data
+		puffs = self.__puffs[tolerance]	
+		
+		for puff in puffs:
+			yield self.__events[puff[0]:puff[1]]
+        
+        
+        
 	def channel(self,i):
 		return self._channels[i]
 	
 	# return projection on the eventdata for this cluster
 	def events(self):
 		if self.__events == None:
-			# the id of this cluster
-			i     = self.__index
-
 			# the indices of the channels within this cluster
-			cidx  = [channel.index() for channel in self.channels()]
+			cidx  = [channel.index() for channel in self]
 			#print cidx
 			# the eventdata from where to extract
 			data  = self.__eventdata._data
 
-			initialstate = data['states'][0,cidx]
-			#print initialstate
+			#select all events for this cluster
+			eventmask = data['clid'] == self.__index
+			#select first and last frame
+			eventmask[0]  = True
+			eventmask[-1] = True
 
-			finalstate   = data['states'][-1,cidx]
-			#print finalstate.shape
+			#create recarray that stores the events of this cluster
+			self.__events = numpy.recarray(shape = (eventmask.sum()),dtype = [('t', '<f8'), ('noch', '<i2'), ('states', '|i1', (len(cidx), 8))])
 
-			initialtime  = self.__simulation.tmin()
-			finaltime    = self.__simulation.tmax()
-
-			frames  = data[data['clid'] == i]['t']
-			states  = data[data['clid'] == i]['states'][:,cidx]
-
-			#print states.shape
-
-			#insert initial and final state
-			states  = numpy.insert(states,0, initialstate,axis = 0)
-			states  = numpy.append(states,   [finalstate],axis = 0)
-
-			frames  = numpy.insert(frames,0,initialtime,axis = 0)
-			frames  = numpy.append(frames,  finaltime)
+			# copy time chid and subspace of state column to new recarray
+			self.__events['t']       = data[eventmask]['t']			
+			self.__events['states']  = data[eventmask]['states'][:,cidx,:]
 			
-			#self.__state = TimeLine(frames,states,interpolationorder = 'zero')
-			self.__events = (frames,states)
+			# cache the number of open channels
+			opencondition = self.__eventdata._states['open']['condition']
+			self.__events['noch']    = numpy.apply_along_axis(opencondition,2,self.__events['states']).sum(axis = 1)
 		return self.__events
